0d090afe2239aa63d98fc7c07632251d3e373337
takochuu <takochuu@gmail.com>
2018-08-13 16:34:00 +0900

    Add gorilla mux

--- main.go
+++ main.go
@@ -4,15 +4,17 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 
+	"github.com/gorilla/mux"
 	"github.com/takochuu/twitter-activity-api-boilerprate/interfaces"
 )
 
 func main() {
 
-	http.HandleFunc("/crc_token", func(res http.ResponseWriter, req *http.Request) {
+	r := mux.NewRouter()
+	r.HandleFunc("/crc_token", func(res http.ResponseWriter, req *http.Request) {
 		h := interfaces.NewTwitterCRCCheckHandler()
 		h.GenerateCRCToken(res, req)
-	})
+	}).Methods("GET")
 
-	http.ListenAndServe(":16000", nil)
+	http.ListenAndServe(":8091", r)
 }
